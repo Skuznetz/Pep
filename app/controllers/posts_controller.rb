148fbfa0753fedd83e25085005b1bf828b71089e
kuznetz <s.kuznetzov@mail.ru>
2017-04-01 20:37:18 +0300

    метод update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,20 +18,30 @@ class PostsController < ApplicationController
   	else
   	  render :new
   	end
+  end
 
   def edit
   	@post = Post.find(params[:id])
   end
 
+  def update
+  	@post =Post.find(params[:id])
+  	if @post.update_attributes(post_params)
+  	  redirect_to @post
+  	else
+  	  render :edit
+  	end
   end
+
+  
 
   private
 
   def post_params
   	params.require(:post).permit(:title,:summary,:body)
   end
-
-
-
-
 end
+
+
+
+
