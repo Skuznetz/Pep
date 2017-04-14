1368281aebcd834235e412542b69f9224206605e
Skuznetz <s.kuznetzov@mail.ru>
2017-04-14 19:07:05 +0200

    метод all_tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :title, :summary, :body,presence: true
+
+  def all_tags
+  	self.tags.map(&:name).join(',')
+  end
+
+  def all_tags=(names)
+  	self.tags = names.split(',').map do |name| 
+    Tag.where(name: name.strip).first_or_create!
+  end
+end
 end
