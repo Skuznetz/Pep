51bfc1345eb3bfb2badc7f255c39e6f1da452100
Skuznetz <s.kuznetzov@mail.ru>
2017-04-23 18:12:41 +0200

    метод bootstrap_class_for(name)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,14 @@ module ApplicationHelper
   	result.join(' ').html_safe
     
   end
+
+  def bootstrap_class_for(name)
+    {success: "alert-success",
+     error: "alert-danger",
+     danger: "alert-danger",
+     alert: "alert-warning",
+     notice: "alert-info"
+    }[name.to_sym] || name
+  end
+
 end
