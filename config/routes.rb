b3c674646fa3cba59709c42292640bf67f58a2b0
Skuznetz <s.kuznetzov@mail.ru>
2017-05-06 18:33:41 +0200

    новый маршрут

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do 
   devise_for :users
   root 'posts#index'
-  resources :posts
+  resources :posts,only: [:show,:index]
   resources :pictures, only: [:create, :destroy]
   resources :tags, only: [:show]
   resources :categories,only: [:show]
 
   namespace :admin do 
     resources :categories,except: [:show]
+    resources :posts,except: [:show,:index]
   end
 end
