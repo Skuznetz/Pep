d114aaddfbceb70793660973e22e52445039af5c
Skuznetz <s.kuznetzov@mail.ru>
2017-05-06 19:01:03 +0200

    редактируем posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!,except: [:index,:show]
-  before_action :find_post ,only: [:show,:edit,:update,:destroy]
+  
+  before_action :find_post ,only: :show
 
   def index
   @posts = Post.paginate(page: params[:page],per_page: 3)
@@ -10,41 +10,7 @@ class PostsController < ApplicationController
   	
   end
 
-  def new
-  	@post = Post.new
-  end
   
-  def create
-  	@post =Post.new(post_params)
-  	if @post.save
-  	  redirect_to @post , success: 'Статья создана'   
-  	else
-      flash.now[:danger] ='Статья не создана'
-  	  render :new
-  	end
-  end
-
-  def edit
-  end
-
-  def update
-  	if @post.update_attributes(post_params)
-  	  redirect_to @post,success: 'Статья изменена'
-
-  	else
-      flash.now[:danger ] ='Статья не изменилась'
-  	  render :edit
-  	end
-  end
-
-  def destroy
-  	
-  	@post.destroy
-  	redirect_to posts_path, success: 'Статья удалена'
-  end
-
-  
-
   private
 
   def find_post
@@ -52,11 +18,7 @@ class PostsController < ApplicationController
   	@post = Post.find(params[:id])
   end
 
-  def post_params
-
-  	params.require(:post).permit(:title,:summary,:body,:image,:all_tags,:category_id)
-
-  end
+  
 end
 
 
