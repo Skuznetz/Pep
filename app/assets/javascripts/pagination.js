d024dbc69fa540e05f9bca430139b66642af50b5
Skuznetz <s.kuznetzov@mail.ru>
2017-06-14 20:34:09 +0300

    редактируем файл

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,5 +1,5 @@
 $ ->
-  if $('.pagination').length
+  if $('.pagination').length && $('#posts').length
     $(window).scroll ->
 url = $('.pagination.next a').attr('href')
 if url && $(window).scrollTop() > $(document).height() - $(window).height() - 50
