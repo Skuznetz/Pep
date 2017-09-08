853a59667fce5a47e60d19e689bd7e0408620ffc
Skuznetz <s.kuznetzov@mail.ru>
2017-09-08 21:34:38 +0300

    убираем код rspec

--- config/application.rb
+++ config/application.rb
@@ -8,19 +8,19 @@ Bundler.require(*Rails.groups)
 
 module Pep
   class Application < Rails::Application
-
+     config.load_defaults 5.1
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Moscow'
     config.i18n.default_locale = :ru
     config.generators do |g|
-g.test_framework :rspec,
- fixtures: false,
- view_specs: false,
- helper_specs: false,
- routing_specs: false
- end
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
 
   end
 end
