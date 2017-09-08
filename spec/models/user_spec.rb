443be4d580a9e9334c76c2a43f2b2961dfce894f
Skuznetz <s.kuznetzov@mail.ru>
2017-09-08 21:35:42 +0300

    1 test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
-
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do 
+  it"user должен иметь имя" do 
+    user = User.new(first_name: nil) 
+    user.valid?
+    expect(user.errors[:first_name]).to include("can't be blank")
+  end
+  
+  
 end
